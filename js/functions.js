e4aafa256d9fbf250856cd57561bf03b8abde8df
Gus <gustavo.schiavon@gmail.com>
2020-11-11 19:54:30 -0300

    establish basic functions for scramble and shaka

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-const intro = document.querySelector('.intro');
+const intro = document.querySelector('.shaka');
 const video = intro.querySelector('video');
 const text = intro.querySelector('h1');
 
@@ -8,7 +8,7 @@ const controller = new ScrollMagic.Controller();
 
 // Scenes
 const scene = new ScrollMagic.Scene({
-  duration: 9000,
+  duration: 0,
   triggerElement: intro,
   triggerHook: 0
 })
@@ -18,7 +18,7 @@ const scene = new ScrollMagic.Scene({
 
 // Video Animation
 
-let accelamount = 0.1;
+let accelamount = 0.2;
 let scrollpos = 0;
 let delay = 0;
 
@@ -30,3 +30,62 @@ setInterval(() => {
   delay += (scrollpos - delay) * accelamount;
   video.currentTime = delay;
 }, 33.3);
+
+let lineOneDefault = document.getElementById('line-1')
+let lineTwoDefault = document.getElementById('line-2')
+let lineThreeDefault = document.getElementById('line-3')
+let lineFourDefault = document.getElementById('line-4')
+let lineFiveDefault = document.getElementById('line-5')
+
+const lineOneArray = [
+  'grupo de jovens',
+  'bando de loucos',
+  'coletivo de amigos',
+  "coletivo criativo",
+  "forno de pizza",
+  "terreiro"
+]
+
+const lineTwoArray = [
+  'pizza',
+  'animações e pós-produção',
+  'macumba',
+  'desenvolvimento de idéias'
+]
+const lineThreeArray = [
+  'entre a cruz e a espada',
+  'no meio da pizzaria',
+  'no espaço sideral',
+  'em Curitiba'
+]
+
+const randomArrIndex = function(a) {
+  return Math.floor(Math.random() * a.length);
+}
+
+const randomString = function(a) {
+  return a[randomArrIndex(a)]
+}
+
+let setStringOne = function() {
+  return lineOneDefault.textContent = randomString(lineOneArray)
+}
+let setStringTwo = function() {
+  return lineTwoDefault.textContent = randomString(lineTwoArray)
+}
+let setStringThree = function() {
+  return lineThreeDefault.textContent = randomString(lineThreeArray)
+}
+
+const defaultTextOne = function (i) {
+  i.textContent = " "
+  i.textContent = "estúdio";
+};
+const defaultTextTwo = function (i) {
+  i.textContent = " "
+  i.textContent = "imagens em movimento";
+};
+const defaultTextThree = function (i) {
+  i.textContent = " "
+  i.textContent = "no meio de tudo e mais um pouco";
+};
