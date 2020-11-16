40d80a81979715162dc862da2a613737f114ac3e
Gus <gustavo.schiavon@gmail.com>
2020-11-16 10:51:59 -0300

    create navigation, add footer, create team section

--- js/functions.js
+++ js/functions.js
@@ -62,7 +62,7 @@ gsap.registerPlugin(ScrollTrigger, CSSRulePlugin, CSSPlugin);
 
 gsap.to('.r-to-l', {
   scrollTrigger: {
-    markers: true,
+    // markers: true,
     scrub: 0.8,
     end: "bottom 10%",
     trigger: ".tickers",
@@ -73,7 +73,7 @@ gsap.to('.r-to-l', {
 });
 gsap.to('.l-to-r', {
   scrollTrigger: {
-    markers: true,
+    // markers: true,
     scrub: 0.8,
     end: "bottom 10%",
     trigger: ".tickers",
