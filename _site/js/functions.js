feb4c99c01c6c696fde7109c800d408fc1de7ead
Gus <gustavo.schiavon@gmail.com>
2020-11-19 20:49:58 -0300

    improve code for text scrambler

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -1,53 +1,73 @@
+gsap.registerPlugin(ScrollTrigger, CSSRulePlugin, TextPlugin, MotionPathPlugin);
+
 // Scramble words 
+// How to put this on a JSON file for easier UI?
+const scrambleEls = [
+  {
+    id: "line-1",
+    defaultString: "estúdio",
+    array: [
+      "coletivo",
+      "bando",
+      "monastério",
+      "grupo musical",
+      "forno"
+    ],
+  },
+  {
+    id: "line-2",
+    defaultString: "imagens em movimento",
+    array: [
+      "animação",
+      "publicidade",
+      "pós-produção",
+      "idéias",
+      "criatividade",
+      "pizzas",
+    ],
+  },
+  {
+    id: "line-3",
+    defaultString: "no meio de tudo e mais",
+    array: [
+      "no Brasil",
+      "no multiverso",
+      "na quinta dimensão",
+      "em Atlantis, o continente perdido",
+      "na pizzaria",
+    ],
+  },
+  {
+    id: "line-4",
+    defaultString: "linguagens",
+    array: [
+      "substâncias",
+      "ritmos",
+      "cores",
+      "palavras",
+      "formas",
+      "orégano"],
+  },
+  {
+    id: "line-5",
+    defaultString: "caminhos",
+    array: [
+      "o espaço",
+      "planetas",
+      "dimensões",
+      "hipóteses",
+      "azeitonas"
+    ],
+  },
+  {
+    id: "line-6",
+    defaultString: "no prazo.",
+    array: [
+      "pizza.",
+    ]
+  },
+];
 
-let lineOneDefault = document.getElementById('line-1')
-let lineTwoDefault = document.getElementById('line-2')
-let lineThreeDefault = document.getElementById('line-3')
-let lineFourDefault = document.getElementById('line-4')
-let lineFiveDefault = document.getElementById('line-5')
-let lineSixDefault = document.getElementById('line-6')
-
-const lineOneArray = [
-  'coletivo',
-  'bando',
-  'monastério',
-  'grupo musical',
-  'forno'
-]
-const lineTwoArray = [
-  'animação',
-  'publicidade',
-  'pós-produção',
-  'idéias',
-  'criatividade',
-  'pizzas'
-]
-const lineThreeArray = [
-  'no Brasil',
-  'no multiverso',
-  'na quinta dimensão',
-  'em Atlantis, o continente perdido',
-  'na pizzaria'
-]
-const lineFourArray = [
-  'substâncias',
-  'ritmos',
-  'cores',
-  'palavras',
-  'formas',
-  'orégano'
-]
-
-const lineFiveArray = [
-  'o espaço',
-  'planetas',
-  'dimensões',
-  'hipóteses',
-  'azeitonas'
-]
-const lineSixArray = [
-  'pizza.'
-]
 // Generates a random index number from the array items
 const randomArrIndex = function(a) {
   return Math.floor(Math.random() * a.length);
@@ -56,28 +76,54 @@ const randomArrIndex = function(a) {
 const randomString = function(a) {
   return a[randomArrIndex(a)]
 }
+// renders the default text declared on the data-default-text HTML attribute
+const defaultText = function (el) {
+  // gsap.from(el, {
+  //   duration: 1,
+  //   text: {
+  //     value: ""
+  //   },
+  //   ease: "none",
+  //   start: ""
+  // });
+  el.textContent = el.dataset.defaultText;
+};
 // takes the default text content (o) and the array (a) as attributes and sets the textContent of the DOM element
 const setString = function(o, a) {
-  return o.textContent = randomString(a)
+  o.textContent = randomString(a)
+  gsap.from(o, {   
+    duration: 1,
+    text: {
+      value: randomString(a),
+      padSpace: true,
+    },
+    ease: "power2"
+  }).to(o, {
+    text: {
+      value: ""
+    }
+  })
 }
-// renders the default text declared on the data-default-text HTML attribute
-const defaultText = function (i) {
-  i.textContent = i.dataset.defaultText;
+
+
+// mouse events
+const scrambleText = function (o, a) {
+  o.addEventListener('mouseover', function() {
+    setString(o, a)
+  })
+  o.addEventListener('mouseleave', function() {
+    defaultText(o)
+  });
 };
 
-const tickerOne = document.getElementById('ticker-1');
-const tickerTwo = document.getElementById('ticker-2');
-const tickerThree = document.getElementById('ticker-3');
-const tickerFour = document.getElementById('ticker-4');
+// call the function to iterate on the array
+scrambleEls.forEach(function (el) {
+  let target = document.getElementById(el.id);
+  let array = el.array
+  scrambleText(target, array)
+});
 
-// const appendContent = function(el) {
-  
-// }
-
-// console.log(appendTxt(tickerTwo));
-
-// ticker on scroll
-gsap.registerPlugin(ScrollTrigger, CSSRulePlugin);
+// Ticker on scroll
 // Each of these tweens animates the lines by class right to left and left to right
 const tlOne = gsap
   .timeline({
@@ -100,10 +146,10 @@ const tlOne = gsap
 );
 
 // Above-the-fold items
-let bogotaBubble = CSSRulePlugin.getRule(".atf-content:before");
-let headerText = document.getElementById("header-content")
-let menuNav = document.getElementById("nav");
-let backButton = document.getElementById("back-to-top");
+const bogotaBubble = CSSRulePlugin.getRule(".atf-content:before");
+const headerText = document.getElementById("header-content")
+const menuNav = document.getElementById("nav");
+const backButton = document.getElementById("back-to-top");
 
 const tlTwo = gsap
   .timeline({
@@ -111,12 +157,10 @@ const tlTwo = gsap
       // markers: true,
       trigger: ".atf-content",
       scrub: 0.2,
-      duration: 2,
-      // anticipatePin: 1,
+      duration: 4,
       start: "top 25%",
       end: "bottom 35%",
       toggleActions: "restart none reverse pause",
-      // pin: true,
     },
   });
 
@@ -129,114 +173,4 @@ const tlTwo = gsap
   })
   .to(menuNav, {
     x: 500
-  }, "<")
-//  Large screens (1440px to )
-
-// Must adapt timeline for media queries
-// ScrollTrigger.matchMedia({
-//   "(min-width: 1440px)": function() {
-//     // Makes the bubble go to the outside left of screen
-//     gsap.to(bogotaBubble, {
-//       scrollTrigger: {
-//         trigger: ".atf-content",
-//         // markers: true,
-//         scrub: 0.3,
-//         anticipatePin: 1,
-//         start: "top 20%",
-//         end: "bottom 25%",
-//         toggleActions: "restart none reverse pause",
-//         pin: true,
-//       },
-//       // ease: "elastic.out(1, 0.3)",
-//       cssRule: {
-//         translateX: "-25%",
-//         height: 200,
-//         width: 200,
-//       },
-//     });
-//     // Makes the menu disappear to the right of the screen
-//     gsap.to(menuNav, {
-//       scrollTrigger: {
-//         trigger: ".atf-content",
-//         // markers: true,
-//         scrub: 0.3,
-//         start: "top 20%",
-//         end: "bottom 25%",
-//         toggleActions: "restart none reverse pause",
-//         pin: true,
-//       },
-//       // ease: "bounce",
-//       cssRule: {
-//         translateX: "100%",
-//       },
-//     });
-//     // Animation for the back-to-top button
-//     gsap.to(backButton, {
-//       scrollTrigger: {
-//         // markers: true,
-//         trigger: "#scramble",
-//         scrub: 0.3,
-//         start: "top 90%",
-//         end: "bottom 80%",
-//         toggleActions: "restart none reverse pause",
-//       },
-//       cssRule: {
-//         translateY: "0",
-//       },
-//     });
-//   },
-//   "(min-width: 300px) and (max-width: 660px)": function() {
-//     // Makes the bubble go to the outside left of screen
-//     gsap.to(bogotaBubble, {
-//       scrollTrigger: {
-//         trigger: ".blue-divider",
-//         markers: true,
-//         scrub: 0.3,
-//         anticipatePin: 1,
-//         start: "top 10%",
-//         endTrigger: ".small-divider",
-//         end: "bottom 50%",
-//         toggleActions: "restart none reverse pause",
-//         // pin: true,
-//       },
-//       // ease: "elastic.out(1, 0.3)",
-//       cssRule: {
-//         translateX: "-50%",
-//         height: 50,
-//         width: 50,
-//       },
-//     });
-//     // Makes the menu disappear to the right of the screen
-//     gsap.to(menuNav, {
-//       scrollTrigger: {
-//         trigger: ".blue-divider",
-//         markers: true,
-//         scrub: 0.3,
-//         start: "top 5%",
-//         endTrigger: ".small-divider",
-//         end: "bottom 300px",
-//         toggleActions: "restart none reverse pause",
-//         // pin: true,
-//       },
-//       // ease: "bounce",
-//       cssRule: {
-//         translateX: "100%",
-//       },
-//     });
-//     // Animation for the back-to-top button
-//     gsap.to(backButton, {
-//       scrollTrigger: {
-//         // markers: true,
-//         trigger: "#scramble",
-//         scrub: 0.3,
-//         start: "top 90%",
-//         end: "bottom 80%",
-//         toggleActions: "restart none reverse pause",
-//       },
-//       cssRule: {
-//         translateY: "0",
-//       },
-//     });
-//   }
-// });
-  
+  }, "<");
