d5b53ef5dab64f19040d08fec6a936ab3a544d47
Gus <gustavo.schiavon@gmail.com>
2020-11-27 23:25:07 -0300

    change hand animation script

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -209,3 +209,45 @@ const tlTwo = gsap
     yoyo: true
   });
   tlArrow.from(arrow,{y: 8})
+
+  // Animate image on scroll
+  const canvas = document.getElementById("shaka");
+  const context = canvas.getContext("2d");
+
+  canvas.width = 1158;
+  canvas.height = 770;
+
+  const frameCount = 40;
+  const currentFrame = (index) =>
+    `/assets/stopmotion/${(index + 1).toString().padStart(4, "0")}.jpg`;
+
+  const images = [];
+  const gesture = {
+    frame: 0,
+  };
+
+  for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    images.push(img);
+  }
+
+  gsap.to(gesture, {
+    frame: frameCount - 1,
+    snap: "frame",
+    scrollTrigger: {
+      markers: true,
+      trigger: ".atf-content",
+      duration: 1,
+      scrub: 0.5,
+      start: "top 20%"
+    },
+    onUpdate: render, // use animation onUpdate instead of scrollTrigger's onUpdate
+  });
+
+  images[0].onload = render;
+
+  function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[gesture.frame], 0, 0);
+  }
